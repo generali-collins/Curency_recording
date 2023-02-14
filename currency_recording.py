cb22134a985d3e7c23edea50da9c63dc55119aad
generali.collins <104697851+generali-collins@users.noreply.github.com>
2023-02-14 06:08:24 +0000

    Add files via upload

---
+++ currency_recording.py
@@ -0,0 +1,7 @@
+foodCharge=eval(input("enter the food charge:  "))
+tip=foodCharge*0.18
+print(format(tip, '.2f'))
+salesTax=foodCharge*0.07
+print(format(salesTax, '.2f'))
+total=foodCharge+tip+salesTax
+print(format(total, '.2f'))
